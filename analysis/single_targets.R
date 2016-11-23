5337da18c6adcf253131d013b7a1e0939a3d7bb7
Sam Urmy <samuel.urmy@stonybrook.edu>
2016-11-23 13:07:06 -0500

    calculate fish length and weight from power law regressions

--- analysis/single_targets.R
+++ analysis/single_targets.R
@@ -5,10 +5,15 @@ library(viridis)
 
 load("single_targets.Rdata")
 sound.speed <- 1500
+a <- mean(c(-4.867, -4.898, -5.186))
+b <- mean(c(2.98, 2.99, 3.103))
 
 targets <- filter(targets, TS_uncomp > -60, TS_comp < -30, Target_range < 100)
-# Foote 1987, from Simmonds and MacLennan (in cm)
-targets <- mutate(targets, length = 10^((TS_uncomp + 67.4) / 20)) 
+targets <- mutate(targets, 
+                  # Foote 1987, from Simmonds and MacLennan (in cm)
+                  length = 10^((TS_uncomp + 67.4) / 20),
+                  # convert length to mm for W-L equations
+                  weight = 10^(a + b * log10(length * 10)))
 
 
 ggplot(targets, aes(x=TS_comp, y=..density..)) +
@@ -19,83 +24,38 @@ ggplot(targets, aes(x=length, y=..density..)) +
   geom_histogram() +
   facet_grid(Lake ~ trip) + coord_flip()
 
+ggplot(targets, aes(x=weight, y=..density..)) +
+  geom_histogram() +
+  facet_grid(Lake ~ trip) + coord_flip()
+
 mean.ts.fish <- targets %>%
   group_by(Lake, trip) %>%
   summarise(sigma = mean(10^(TS_comp/10)),
             TS = 10*log10(sigma),
             length = mean(length),
+            weight = mean(weight),
             n = n())
 
 reshape2::dcast(mean.ts.fish, Lake ~ trip, value.var = "TS")
 reshape2::dcast(mean.ts.fish, Lake ~ trip, value.var = "length")
+reshape2::dcast(mean.ts.fish, Lake ~ trip, value.var = "weight")
 reshape2::dcast(mean.ts.fish, Lake ~ trip, value.var = "n")
 
+mean.ts.fish <- arrange(mean.ts.fish, trip, Lake)
 write.csv(mean.ts.fish, file = "fish_TS.csv", row.names = F)
 
-
-# 
-# filter(targets, Lake %in% c("Cherry", "Eleanor")) %>%
-# ggplot(aes(x=Target_longitude, y=Target_latitude)) +
-#   geom_point(size=0.5, alpha=0.5) + 
-#   facet_wrap(~trip)
-# 
-# target.density <- targets %>%
-#   mutate(range = round(Target_range),
-#          pulse.length = Transmitted_pulse_length * 1e-3 * sound.speed,
-#          radius = range * tan(3.5 * pi / 180) + 0.2,
-#          volume = pi * radius^2 * pulse.length) %>%
-#   group_by(trip, Lake, Ping_number, range) %>%
-#   summarize(TS_mean = mean(TS_comp),
-#             volume = mean(volume),
-#             count = n(),
-#             density = count / volume)
-# 
-# 
-# ggplot(target.density, aes(y=-range, x=Ping_number, size=density, color=TS_mean)) +
-#   geom_point(alpha=0.3) + facet_grid(Lake ~ trip) 
-# 
-# 
-# target.density <- target.density %>%
-#   group_by(trip, Lake, range) %>%
-#   summarize(count = mean(count),
-#             volume = mean(volume))
-# 
-# ggplot(target.density, aes(x=-range, y=count)) + 
-#   geom_point() + geom_smooth(method="gam", method.args=list(family="poisson"))+
-#   coord_flip() + facet_grid(Lake ~ trip)
-# 
-# area.density <- targets %>%
-#   group_by(trip, Lake, Ping_number) %>%
-#   summarize(max_depth = max(Target_range),
-#             radius = max_depth * tan(3.5 * pi / 180),
-#             volume = pi/3 * radius^2 * max_depth,
-#             count = n(),
-#             density = count / volume,
-#             area.density = density * max_depth,
-#             lon = mean(Target_longitude),
-#             lat = mean(Target_latitude))
-# 
-# filter(area.density, Lake %in% c("Cherry", "Eleanor")) %>%
-#   ggplot(aes(x=lon, y=lat, size=count, color=volume)) +
-#   geom_point() + scale_color_viridis() + facet_wrap(~trip)
-# 
-# ggplot(area.density, aes(x=count, y=..density..)) +
-#   geom_histogram(binwidth=1) + facet_grid(Lake ~ trip)
-# 
-# ggplot(area.density, aes(x=density, y=..density..)) +
-#   geom_histogram() + xlim(0, 2) + facet_grid(Lake ~ trip)
-# 
-# area.density %>%
-#   group_by(trip, Lake) %>%
-#   summarize(Density = mean(density)) %>% 
-#   ggplot(aes(x=trip, y=Density, color=Lake)) + geom_point()
-# 
-# area.density %>%
-#   group_by(trip, Lake) %>%
-#   summarize(Density = mean(density)) %>% 
-#   spread(trip, Density)
-# 
-# area.density %>%
-#   group_by(trip, Lake) %>%
-#   summarize(SD = sd(density)) %>%
-#   spread(trip, SD)
+ts.display.table.fish <- targets %>%
+  filter(Lake != "Independence") %>%
+  group_by(trip, Lake) %>%
+  summarise(TS.mean = 10*log10(mean(10^(TS_comp/10))),
+            TS.sd = sd(TS_comp),
+            length.mean = mean(length),
+            length.sd = sd(length),
+            weight.mean = mean(weight),
+            weight.sd = sd(weight),
+            n = n())
+ts.display.table.fish[, 3:8] <- signif(ts.display.table.fish[ , 3:8], 2)
+for (col in c("TS.sd", "length.sd", "weight.sd")) {
+  ts.display.table.fish[[col]] <- paste0("(", ts.display.table.fish[[col]], ")")
+}
+write.csv(ts.display.table.fish, "nets/ts.display.table.fish.csv")
