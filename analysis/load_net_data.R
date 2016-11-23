7faa72ca4233b5c3a820861e606436a69a138807
Sam Urmy <samuel.urmy@stonybrook.edu>
2016-11-23 13:09:32 -0500

    clean up some strings/factors warning messages

--- analysis/load_net_data.R
+++ analysis/load_net_data.R
@@ -3,22 +3,21 @@ library(tidyr)
 library(reshape2)
 library(stringr)
 
-net.meta <- read.csv("nets/RimFireZooplanktonSampleList.csv") %>%
+net.meta <- read.csv("nets/RimFireZooplanktonSampleList.csv", stringsAsFactors = F) %>%
   filter(SplitNumber %in% c(0, 2) | Year == 2013) %>%
   mutate(trip = paste0(Year, "-", str_pad(Month, 2, pad="0")),
          SplitNumber = as.integer(as.character(SplitNumber))) %>%
   select(trip, Lake, MaxDepth, NetType, SplitNumber, VolFiltered, 
          Biovolume, TERCSplitFactor, Dilution=TotalDilutionFactor)
 
-individuals <- read.csv("nets/RimFireZooplanktonMeasurements.csv")
-levels(individuals$Month) <- c(4, 6, 10, 9)
+individuals <- read.csv("nets/RimFireZooplanktonMeasurements.csv", stringsAsFactors = F)
+individuals$Month <- plyr::revalue(individuals$Month, c(Sept="09", Oct="10", April="04", June="06"))
 individuals <- mutate(individuals, 
-                      Month = as.integer(as.character(Month)),
                       Group = gsub("Cladocera", "Cladocerans", Group),
                       Group = gsub("Penilia", "Cladocerans", Group))
 
 
-counts <- read.csv("nets/RimFireZooplanktonCounts.csv", skip=5) %>%
+counts <- read.csv("nets/RimFireZooplanktonCounts.csv", skip=5, stringsAsFactors = F) %>%
   mutate(SplitNumber = gsub("S", "", SplitNumber)) %>%
   melt(id.vars=1:8, variable.name="taxon.code", value.name="Count") %>%
   mutate(taxon.code = as.character(taxon.code)) %>%
@@ -27,7 +26,7 @@ counts <- read.csv("nets/RimFireZooplanktonCounts.csv", skip=5) %>%
 counts$taxon.code[counts$taxon.code == "X"] <- "X.0"
 
 taxa <- read.csv("nets/RimFireZooplanktonCounts.csv", nrows=5, header=F) %>%
-  t() %>% as.data.frame() %>%
+  t() %>% as.data.frame(stringsAsFactors=F) %>%
   slice(9:n()) %>%
   rename(Group = V1, Order = V2, Genus = V3, Species = V4, LifeStage = V5) %>%
   mutate(Group = gsub("Penilia", "Cladocera", Group),
