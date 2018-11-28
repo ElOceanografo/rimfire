bc2546dc560bce17af0808af47884acebfe0a611
Sam Urmy <oceanographerschoice@gmail.com>
2018-11-28 13:16:48 -0800

    CTD plots w/ Indy and Tahoe

--- analysis/ctd.R
+++ analysis/ctd.R
@@ -21,18 +21,42 @@ cols <- list("timeS", "prSM", "t090C", "c0uS", "bat", "xmiss", "wetStar",
 
 ctd <- plyr::ddply(ctd.files, c("trip", "Lake"), function(df) {
     read.csv(df$filename, header=F, skip=242, comment.char="#", col.names=cols)
-  })
+  }) %>%
+  select(trip, Lake, prSM, t090C)
+names(ctd) <- c("trip", "Lake", "Depth", "Temperature")
+
+ctd.tahoe <- read.csv("ctd/LTP1444_down.txt", skip=137, header=F) %>%
+  mutate(trip="2013-10", Lake="Tahoe") %>%
+  select(trip, Lake, V2, V3)
+names(ctd.tahoe) <- c("trip", "Lake", "Depth", "Temperature")
 
 
-ggplot(ctd, aes(x=t090C, y=prSM, linetype=Lake)) +
-  geom_path() + 
+ctd.indy <- read.csv("ctd/Independenc_EXO_SD_13C100952_102013_212309.csv") %>%
+  mutate(trip = "2013-10", Lake="Independence") %>%
+  select(trip, Lake, Depth.m, Temp..C)
+names(ctd.indy) <- c("trip", "Lake", "Depth", "Temperature")
+
+ctd <- rbind(ctd, ctd.indy, ctd.tahoe) %>%
+  mutate(Depth = round(Depth)) %>%
+  group_by(trip, Lake, Depth) %>%
+  summarize(Temperature = mean(Temperature, na.rm=T)) %>%
+  ungroup()
+
+# 2-m bins
+binwidth <- 3
+ctd.binned <- ctd %>%
+  mutate(Depth = binwidth * round(Depth/binwidth)) %>%
+  group_by(trip, Lake, Depth) %>%
+  summarize(Temperature = mean(Temperature, na.rm=T)) %>%
+  ungroup()
+
+p <- ggplot() +
+  geom_path(aes(x=Temperature, y=Depth, linetype=Lake), data=ctd) +
+  # geom_point(aes(x=Temperature, y=Depth, shape=Lake), data=ctd.binned) +
   scale_y_reverse("Depth (m)", limits=c(50, 0)) +
-  xlab(expression(Temperature~("°C"))) +
+  scale_x_continuous(expression(Temperature~("°C"))) +
   facet_grid(. ~ trip) + theme_minimal()
+p
+ggsave("graphics/ctd.png", p)
 
 
-ggplot(ctd, aes(x=bat, y=prSM, linetype=Lake)) +
-  geom_path() + 
-  scale_y_reverse("Depth (m)", limits=c(50, 0)) +
-  xlab(expression(Attenuation~(m^-1))) +
-  facet_grid(. ~ trip) + theme_minimal()
